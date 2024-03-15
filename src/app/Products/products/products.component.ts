b2428e103eb7d4e7ebb0c6dd91d2b445eeecc5bf
Sefinsha-b <Sefinsha4@gmail.com>
2024-03-15 13:05:35 +0530

    uiupdate

--- src/app/Products/products/products.component.ts
+++ src/app/Products/products/products.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
 
   products = [
-    { name: 'Product 1', price: 10, imageUrl: 'assets/logo/car.svg' },
+    { name: 'Product 1', price: 10, imageUrl: 'assets/logo/Mask group.svg' },
     { name: 'Product 2', price: 20, imageUrl: 'assets/product2.jpg' },
     { name: 'Product 3', price: 15, imageUrl: 'assets/product3.jpg' },
     { name: 'Product 1', price: 10, imageUrl: 'assets/product1.jpg' },
