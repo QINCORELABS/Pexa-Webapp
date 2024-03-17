d1ebc3fca47b9fdb03a15413ae227c3f7b09304d
Sefinsha-b <Sefinsha4@gmail.com>
2024-03-18 03:38:09 +0530

    More Ui Updates

--- src/app/shared/navwhitehead/navwhitehead.component.ts
+++ src/app/shared/navwhitehead/navwhitehead.component.ts
@@ -9,5 +9,22 @@ export class NavwhiteheadComponent implements OnInit {
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
- 
+  
+  searchQuery: string = '';
+  showList: boolean = false;
+  showSearch: boolean = true;
+
+
+  toggleList(): void {
+    this.showList = !this.showList;
+    this.showSearch = true;
+  }
+
+
+  toggleSearch(): void {
+    this.showSearch = false;
+  }
+
+
+
 }
